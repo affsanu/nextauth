49413151be5cdce905e9cd06e5362e3c960bd162
Sanu Islam <sanuvipbd@gmail.com>
2023-05-19 16:18:13 +0600

    Next Config Update

--- next.config.js
+++ next.config.js
@@ -15,12 +15,7 @@ const nextConfig = {
       topLevelAwait: true,
     };
     return config;
-  },
-  env: {
-    MONGO_URI:
-      "mongodb+srv://nextapp:nextapp@next-app.6i1ft8m.mongodb.net/Acc?retryWrites=true&w=majority",
-  },
-  JWT_SECRET: "899c9b5b0469bcff67177b39eb46bd0e",
+  }
 };
 
 module.exports = nextConfig
