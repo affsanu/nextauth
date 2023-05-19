fef2cd425a95870179f67b5415b6962832830dfe
Sanu Islam <sanuvipbd@gmail.com>
2023-05-19 16:35:19 +0600

    modify registration server side

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -6,23 +6,23 @@ export async function POST(req) {
   try {
     await DbConnect();
 
-    const { username, email, password: pass } = await req.json();
+    const { username, email, password } = await req.json();
 
     const isExisting = await User.findOne({ email });
     if (isExisting) {
       throw new Error("User already exists!");
     }
 
-    const hashedPassword = await bcrypt.hash(pass, 10);
-    const newUser = await User.create({
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await User.create({
       username,
       email,
       password: hashedPassword,
     });
 
-    const { password, ...user } = newUser._doc;
-
-    return new Response(JSON.stringify(user), { status: 201 });
+    return new Response(JSON.stringify({ msg: "User registration success!" }), {
+      status: 201,
+    });
   } catch (error) {
     return new Response(JSON.stringify(error.message), { status: 500 });
   }
